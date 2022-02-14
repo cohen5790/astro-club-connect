1c763df4776f5de579af728f0934b2bf33da9636
Eric Peterson <ericpeterson91@live.ca>
2022-02-13 19:13:30 -0600

    testing branches

---
+++ AstroClub/main_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add_photo/', views.add_photo, name='add_photo')
+]
--- AstroClub/main_app/views.py
+++ AstroClub/main_app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Photo
+import uuid
+import boto3
 
+S3_BASE_URL = 'https://s3-ca-central-1.amazonaws.com/' 
+BUCKET = 'astro-club-bucket'
+
+
+def index(request):
+    return render(request, 'profile.html')
+
+def add_photo(request):
+    # photo-file will be the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to cat_id or cat (if you have a cat object)
+            photo = Photo(url=url)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('profile/')
 # Create your views here.
