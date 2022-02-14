5a7379f5dbcbcad87b6b224f739b89388fe18307
Eric Peterson <ericpeterson91@live.ca>
2022-02-14 01:26:15 -0600

    small edits

--- AstroClub/main_app/urls.py
+++ AstroClub/main_app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('profile/', views.profile, name="profile"),
+    path('matches/', views.matches, name="matches"),
     path('accounts/', views.login, name="login"),
     path('accounts/signup', views.signup, name="signup"),
     path('intro_one/', views.intro_one, name="intro_one"),
@@ -13,7 +14,7 @@ urlpatterns = [
     path('questions_one/', views.questions_one, name="questions_one"),
     path('questions_two/', views.questions_two, name="questions_two"),
     path('questions_three/', views.questions_three, name="questions_three"),
-    path('matches/', views.matches, name="matches"),
+    path('questions_matches/', views.questions_matches, name="questions_matches"),
 
 
 
