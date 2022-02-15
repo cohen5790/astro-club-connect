51d6a948a477b42e60f407e13356668bd39ef1c4
cohen5790 <cohen5790@gmail.com>
2022-02-15 00:36:11 -0500

    database on admin site

--- AstroClub/main_app/admin.py
+++ AstroClub/main_app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 
-from .models import User, Photo
+from .models import User, Photo, Profile
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Photo)
+admin.site.register(Profile)
--- AstroClub/main_app/models.py
+++ AstroClub/main_app/models.py
@@ -1,3 +1,4 @@
+# from socket import PACKET_HOST
 from django.db import models
 from django.urls import reverse
 
@@ -13,7 +14,43 @@ class Photo(models.Model):
 class User(models.Model):
     name = models.CharField(max_length=100)
 
-    def __str__(self):
-        return f'hello {self}'
+    # def __str__(self):
+    #     return f'hello {self}'
 
-   
+class Profile(models.Model):
+    CAPRICORN = 'CA'
+    TAURUS = 'TA'
+    GEMINI = 'GE'
+    CANCER = 'CA'
+    LEO = 'LE'
+    VIRGO = 'VI'
+    LIBRA = 'LI'
+    SCORPIO = 'SC'
+    SAGITTARIUS = 'SA'
+    AQUARIUS = 'AQ'
+    PISCES = 'PI'
+    HOROSCOPE_CHOICES = [
+        (CAPRICORN, 'Capricorn'),
+        (TAURUS, 'Taurus'),
+        (GEMINI, 'Gemini'),
+        (CANCER, 'Cancer'),
+        (LEO, 'Leo'),
+        (VIRGO, 'Virgo'),
+        (LIBRA, 'Libra'),
+        (SCORPIO, 'Scorpio'),
+        (SAGITTARIUS, 'Sagittarius'),
+        (AQUARIUS, 'Aquarius'),
+        (PISCES, 'Pisces'),
+    ]
+    user_name = models.CharField(max_length=20),
+    password = models.CharField(max_length=20),
+    first_name = models.CharField(max_length=100),
+    last_name = models.CharField(max_length=100),
+    profile_pic = models.ForeignKey(Photo, on_delete=models.SET_NULL),
+    horoscope = models.CharField(
+        max_length=2,
+        choices=HOROSCOPE_CHOICES,
+        default='CAPRICORN',
+        )
+    
+
